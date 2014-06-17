53f013f21c1ffbd988e887a901188796b28baa9b
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-17 03:15:02 -0700

    Add a scrollview and two additional views to the scroll view.

--- Hypnosister/Hypnosister/AppDelegate.swift
+++ Hypnosister/Hypnosister/AppDelegate.swift
@@ -16,9 +16,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
 
         // Override point for customization after application launch.
-        let firstFrame = self.window!.bounds
-        let firstView  = HypnosisView(frame: firstFrame)
-        self.window!.addSubview(firstView)
+        var screenRect = self.window!.bounds
+        var bigRect = screenRect
+        bigRect.size.width *= 2.0
+
+        // Create a screen sized scroll
+        let scrollView = UIScrollView(frame: screenRect)
+        scrollView.pagingEnabled = true
+        self.window!.addSubview(scrollView)
+
+        // Add a screen sized Hypnosis view and add it to the scroll view
+        let hypnosisView = HypnosisView(frame: screenRect)
+        scrollView.addSubview(hypnosisView)
+
+        // Add a second screen-sized hypnosis view
+        screenRect.origin.x += screenRect.size.width
+        let anotherView = HypnosisView(frame: screenRect)
+        scrollView.addSubview(anotherView)
+
+        // Scroll view == bigRect's size
+        scrollView.contentSize = bigRect.size
+
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
         
