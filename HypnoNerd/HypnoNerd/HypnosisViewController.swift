bf9b40fd124646e665d5b95575725b0253073d97
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-18 02:50:14 -0700

    Simplify init to call the super's designated init instead.

--- HypnoNerd/HypnoNerd/HypnosisViewController.swift
+++ HypnoNerd/HypnoNerd/HypnosisViewController.swift
@@ -1,14 +1,12 @@
 import UIKit
 
 class HypnosisViewController: UIViewController {
-    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    init() {
+        super.init(nibName: nil, bundle: nil)
         tabBarItem.title = "Hypnotize"
-        tabBarItem.image = UIImage(named: "Hypno@2x.png")
+        tabBarItem.image = UIImage(named: "Hypno.png")
     }
-
     override func loadView() {
         view = HypnosisView()
     }
-
 }
