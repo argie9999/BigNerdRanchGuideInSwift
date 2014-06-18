430e0c74e9a5b4e6f3e62783b6b1751bdac129ac
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-18 02:50:44 -0700

    Extend UILocalNotification to add a convenience initalizer.
    - Also send local notifications to the user.

--- HypnoNerd/HypnoNerd/ReminderViewController.swift
+++ HypnoNerd/HypnoNerd/ReminderViewController.swift
@@ -1,16 +1,28 @@
 import UIKit
 
+// Extend UILocalNotification for fun.
+extension UILocalNotification {
+    convenience init(alert: String, withDate date: NSDate) {
+        self.init()
+        alertBody = alert
+        fireDate = date
+    }
+}
+
 class ReminderViewController: UIViewController {
     @IBOutlet weak var datePicker: UIDatePicker
-
-    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    init() {
+        super.init(nibName: nil, bundle: nil)
         tabBarItem.title = "Reminder"
         tabBarItem.image = UIImage(named: "Time.png")
     }
 
     @IBAction func addReminder(sender: AnyObject) {
         let date = datePicker.date
+        // Use that cool extension.
         NSLog("Setting a reminder for %@", date)
+        // Using the cool extension to UILocalNotification defined above.
+        let note = UILocalNotification(alert: "Hypnotize me!", withDate: date)
+        UIApplication.sharedApplication().scheduleLocalNotification(note)
     }
 }
