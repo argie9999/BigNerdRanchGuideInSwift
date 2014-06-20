65b714c64b2e7ba04dfb1b3a38e4b3a28217d5b5
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-19 17:26:56 -0700

    Add parallax effect to message label!

--- HypnoNerd/HypnoNerd/HypnosisViewController.swift
+++ HypnoNerd/HypnoNerd/HypnosisViewController.swift
@@ -15,7 +15,7 @@ class HypnosisViewController: UIViewController, UITextFieldDelegate {
         tabBarItem.title = "Hypnotize"
         tabBarItem.image = UIImage(named: "Hypno.png")
     }
-
+    
     override func loadView() {
         let backgroundView = HypnosisView(frame: UIScreen.mainScreen().bounds)
         let textField = UITextField(frame: CGRectMake(40, 70, 240, 30))
@@ -24,42 +24,56 @@ class HypnosisViewController: UIViewController, UITextFieldDelegate {
         textField.returnKeyType = UIReturnKeyType.Done
         textField.delegate = self
         backgroundView.addSubview(textField)
-
+        
         view = backgroundView
     }
-
+    
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
         drawHypnoticMessage(textField.text)
         textField.text = ""
         textField.resignFirstResponder()
-
+        
         return true
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NSLog("HypnosisViewController loaded!")
     }
-
+    
     func drawHypnoticMessage(message: String) {
         for _ in 0..20 {
             // use the cool extension to UILabel defined above.
             let messageLabel = UILabel(backgroundColor: UIColor.clearColor(), text: message)
             messageLabel.sizeToFit()
-
+            
             // Get random x value inside HynosisView
             let width = UInt32(view.bounds.size.width - messageLabel.bounds.size.width)
             let x = arc4random() % width
-
+            
             // Get random y value inside HypnosisView
             let height = UInt32(view.bounds.size.height - messageLabel.bounds.size.height)
             let y = arc4random() % height
-
+            
             var frame = messageLabel.frame
             frame.origin = CGPointMake(CGFloat(x), CGFloat(y))
             messageLabel.frame = frame
-
+            
             view.addSubview(messageLabel)
+            
+            // Add parallax effect along the horizontal axis
+            var motionEffect = UIInterpolatingMotionEffect(keyPath: "center.x",
+                type: UIInterpolatingMotionEffectType.TiltAlongHorizontalAxis)
+            motionEffect.minimumRelativeValue = -25
+            motionEffect.maximumRelativeValue = 25
+            messageLabel.addMotionEffect(motionEffect)
+
+            // Along the vertical axis now
+            motionEffect = UIInterpolatingMotionEffect(keyPath: "center.y",
+                type: UIInterpolatingMotionEffectType.TiltAlongVerticalAxis)
+            motionEffect.minimumRelativeValue = -25
+            motionEffect.maximumRelativeValue = 25
+            messageLabel.addMotionEffect(motionEffect)
         }
     }
 }
