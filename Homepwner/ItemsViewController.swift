cfd6e4cd0056202f38d9e5fa794b809fbbede494
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-20 01:52:07 -0700

    Much better.

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -1,15 +1,7 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
-    init() {
-        super.init(style: UITableViewStyle.Plain)
-    }
-
-    convenience init(style: UITableViewStyle) {
-        self.init()
-    }
-
-    convenience init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
-        self.init(nibName: nil, bundle: nil)
+    convenience init() {
+        self.init(style: .Plain)
     }
 }
