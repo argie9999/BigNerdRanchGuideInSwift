ebac5c8f9cc5fbbc71bcf0ad28150445712d1eb5
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-19 17:16:09 -0700

    Whitespace

--- HypnoNerd/HypnoNerd/AppDelegate.swift
+++ HypnoNerd/HypnoNerd/AppDelegate.swift
@@ -3,7 +3,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+
     func application(application: UIApplication, didFinishLaunchingWithOptions
         launchOptions: NSDictionary?) -> Bool
     {
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
-        
+
         return true
     }
 }
