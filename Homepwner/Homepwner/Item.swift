663c58792213486779303785ee03dc5c3dd66e84
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-20 02:22:45 -0700

    Add Item class for the data source for table view.

---
+++ Homepwner/Homepwner/Item.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class Item {
+    let itemName: String
+    let serialNumber: String
+    let valueInDollars: Int
+    let dateCreated: NSDate
+
+    // Designated Initializer
+    init(itemName name: String, valueInDollars: Int, serialNumber: String) {
+        self.itemName = name
+        self.serialNumber = serialNumber
+        self.valueInDollars = valueInDollars
+        dateCreated = NSDate()
+    }
+
+    convenience init(itemName name: String) {
+        self.init(itemName: name, valueInDollars: 0, serialNumber: "")
+    }
+
+    convenience init() {
+        self.init(itemName: "Item")
+    }
+
+    class func randomItem() -> Item {
+        let randomAdjectiveList = ["Fluffy", "Rusty", "Shiny"]
+        let randomNounList = ["Bear", "Spork", "Mac"]
+        let adjectiveIndex = Int(arc4random()) % randomAdjectiveList.count
+        let nounIndex = Int(arc4random()) % randomNounList.count
+
+        let randomName = "\(randomAdjectiveList[adjectiveIndex]) \(randomNounList[nounIndex])"
+        let randomVal = Int(arc4random()) % 100
+        let randomSerial =
+            "\(arc4random() % 10)\(arc4random() % 26)\(arc4random() % 10)\(arc4random() % 26)\(arc4random() % 10)"
+
+        return Item(itemName: randomName, valueInDollars: randomVal, serialNumber: randomSerial)
+    }
+}
