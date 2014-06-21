13e24a79d3b56fed612d5cd52fa1910cf84f1af9
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-21 14:56:44 -0700

    Add a header view for the UITableView.

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -1,26 +1,55 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
+    // Since we can't have a strong optional type, this is a workaround.
+    // Could be better?
+    var _headerView: UIView? = nil
+    @IBOutlet strong var headerView: UIView {
+    get {
+        if !_headerView {
+            NSBundle.mainBundle().loadNibNamed("HeaderView", owner: self, options: nil)
+        }
+        return _headerView!
+    }
+    set {
+        _headerView = newValue
+    }
+    }
+    
     convenience init() {
         self.init(style: .Plain)
         for _ in 0..5 {
             ItemStore.sharedStore.createItem()
         }
     }
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        tableView.tableHeaderView = headerView
+    }
+    
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
         return ItemStore.sharedStore.allItems.count
     }
-
+    
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        let cell = UITableViewCell(style: .Default, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath) as UITableViewCell
         let items = ItemStore.sharedStore.allItems
         cell.textLabel.text = items[indexPath.row].description()
-
+        
         return cell
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    @IBAction func addNewItem(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        
     }
 }
