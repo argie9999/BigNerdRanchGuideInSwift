33dc4530b34c517a8b487907008bfb14975947b7
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-21 00:29:08 -0700

    Finally manage to load data into UITableView

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -4,7 +4,19 @@ class ItemsViewController: UITableViewController {
     convenience init() {
         self.init(style: .Plain)
         for _ in 0..5 {
-            let item = ItemStore.sharedStore
+            ItemStore.sharedStore.createItem()
         }
+    }
+
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return ItemStore.sharedStore.allItems.count
+    }
+
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "UITableViewCell")
+        let items = ItemStore.sharedStore.allItems
+        cell.textLabel.text = items[indexPath.row].description()
+
+        return cell
     }
 }
