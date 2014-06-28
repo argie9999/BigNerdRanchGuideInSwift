43ef83c978884b1a24b635842a6640411db11b95
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-27 20:37:43 -0700

    Add color instance for Line class.

--- TouchTracker/TouchTracker/Line.swift
+++ TouchTracker/TouchTracker/Line.swift
@@ -12,11 +12,13 @@ class Line: Equatable {
     var begin: CGPoint
     var end: CGPoint
     var thickness: Double
+    var color: UIColor
 
     init(begin: CGPoint, end: CGPoint) {
         self.begin = begin
         self.end = end
         thickness = 1.0
+        color = UIColor.blackColor()
     }
 }
 
