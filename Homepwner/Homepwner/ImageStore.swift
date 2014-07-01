e7aecbd6382c497d57ba46348dbe435af1f93df6
Akshay Hegde <akshay.hegde17@gmail.com>
2014-06-30 18:36:28 -0700

    Make ImageStore's instance thread safe.

--- Homepwner/Homepwner/ImageStore.swift
+++ Homepwner/Homepwner/ImageStore.swift
@@ -3,13 +3,15 @@ import UIKit
 class ImageStore: NSObject {
     var dictionary = Dictionary<NSString, UIImage>()
 
+    // Thread-safe read-only instance.
     class var sharedStore: ImageStore {
-        return SharedStore
+        struct Static {
+            static let instance: ImageStore = ImageStore()
+        }
+        return Static.instance
     }
 
     init() {
         NSException(name: "Singleton", reason: "Use ItemStore.sharedStore", userInfo: nil)
     }
 }
-
-let SharedStore = ImageStore()
