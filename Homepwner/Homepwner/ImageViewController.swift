0fe1c61f1ed0ca2df00a05dae8a7fd665e52a150
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-01 20:10:31 -0700

    Add ImageViewController to handle showing the full size image from thumbnail

---
+++ Homepwner/Homepwner/ImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.swift
+//  Homepwner
+//
+//  Created by ajh17 on 7/1/14.
+//  Copyright (c) 2014 Akshay Hegde. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+}
