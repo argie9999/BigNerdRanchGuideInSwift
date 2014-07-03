154a853c520718d9da836a13e2442a172be71a9c
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-02 21:42:18 -0700

    Make the thumbnail for the Item also scale with the Dynamic Type size.

--- Homepwner/Homepwner/ItemCell.swift
+++ Homepwner/Homepwner/ItemCell.swift
@@ -13,6 +13,8 @@ class ItemCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel
     @IBOutlet weak var serialNumberLabel: UILabel
     @IBOutlet weak var valueLabel: UILabel
+    @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint
+    @IBOutlet weak var imageViewWidthConstraint: NSLayoutConstraint
     var actionBlock: (() -> ())?
 
     @IBAction func showImage(id: UIButton) {
@@ -29,6 +31,27 @@ class ItemCell: UITableViewCell {
         let font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
 
         (nameLabel.font, serialNumberLabel.font, valueLabel.font) = (font, font, font)
+
+        struct Static {
+            static let imageSizeDict: Dictionary<NSString, Int> = [
+                UIContentSizeCategoryExtraSmall: 40,
+                UIContentSizeCategorySmall: 40,
+                UIContentSizeCategoryMedium: 40,
+                UIContentSizeCategoryLarge: 40,
+                UIContentSizeCategoryExtraLarge: 45,
+                UIContentSizeCategoryExtraExtraLarge: 55,
+                UIContentSizeCategoryExtraExtraExtraLarge: 65,
+            ]
+        }
+
+        let userSize = UIApplication.sharedApplication().preferredContentSizeCategory
+        let imageSize = Static.imageSizeDict[userSize]
+
+        // Just to be safe.
+        if let imgSize = imageSize {
+            imageViewHeightConstraint.constant = CGFloat(imgSize)
+            imageViewWidthConstraint.constant = CGFloat(imgSize)
+        }
     }
 
     override func awakeFromNib() {
