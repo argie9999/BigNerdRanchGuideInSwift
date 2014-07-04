12be0141d12c63fb5ad2f6b6f0f9adbb71a0738c
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-04 00:09:44 -0700

    Implemented silver challenge bonus (UIToolbar currently doesn't work)

--- Nerdfeed/Nerdfeed/WebViewController.swift
+++ Nerdfeed/Nerdfeed/WebViewController.swift
@@ -26,16 +26,12 @@ class WebViewController: UIViewController {
 
     func goBack(sender: UIBarButtonItem) {
         println("Back button clicked")
-        if (view as UIWebView).canGoBack {
-            (view as UIWebView).goBack()
-        }
+        (view as UIWebView).goBack()
     }
 
     func goForward(sender: UIBarButtonItem) {
         println("Forward button clicked")
-        if (view as UIWebView).canGoForward {
-            (view as UIWebView).goForward()
-        }
+        (view as UIWebView).goForward()
     }
 
     override func loadView() {
@@ -44,11 +40,15 @@ class WebViewController: UIViewController {
         view = webView
 
         // Silver challenge: Add a UIToolbar instance for back and forward buttons
-        let backButton = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "goBack:")
-        let forwardButton = UIBarButtonItem(title: "Forward", style: .Plain, target: self, action: "goForward:")
         toolbar = UIToolbar(frame: CGRectMake(0, 70, view.bounds.width, 44))
-        toolbar!.items = [backButton, forwardButton]
 
+        let backButton = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "goBack:")
+        backButton.enabled = webView.canGoBack ? true : false
+
+        let forwardButton = UIBarButtonItem(title: "Forward", style: .Plain, target: self, action: "goForward:")
+        forwardButton.enabled = webView.canGoForward ? true : false
+
+        toolbar!.items = [backButton, forwardButton]
         view.addSubview(toolbar)
     }
 }
