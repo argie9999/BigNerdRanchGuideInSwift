ad2dcc4cb0afeb5b7f88edb69222924159212b45
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-04 02:42:40 -0700

    Start working on Chapter 21 gold challenge, add a custom tableviewcell

---
+++ Nerdfeed/Nerdfeed/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  Nerdfeed
+//
+//  Created by Akshay Hegde on 7/4/14.
+//  Copyright (c) 2014 Akshay Hegde. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    // MARK: Outlets
+    @IBOutlet var titleLabel: UILabel
+    @IBOutlet var upcomingLabel: UILabel
+}
