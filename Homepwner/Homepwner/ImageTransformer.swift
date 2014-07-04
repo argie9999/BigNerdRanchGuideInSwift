5f046b7668de9c45dc566fddfbea5b37eb53afba
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-04 15:54:25 -0700

    Add an ImageTransformer class to transform and store thumbnail image as Core data

---
+++ Homepwner/Homepwner/ImageTransformer.swift
@@ -0,0 +1,30 @@
+//
+//  ImageTransformer.swift
+//  Homepwner
+//
+//  Created by Akshay Hegde on 7/4/14.
+//  Copyright (c) 2014 Akshay Hegde. All rights reserved.
+//
+
+import UIKit
+
+class ImageTransformer: NSValueTransformer {
+
+    func transformedValueClass() -> AnyClass! {
+        return NSData.classForCoder()
+    }
+
+    override func transformedValue(value: AnyObject!) -> AnyObject! {
+        if !value {
+            return nil
+        }
+        if let val = value as? NSData {
+            return val
+        }
+        return UIImagePNGRepresentation(value as UIImage)
+    }
+
+    override func reverseTransformedValue(value: AnyObject!) -> AnyObject! {
+        return UIImage(data: value as? NSData)
+    }
+}
