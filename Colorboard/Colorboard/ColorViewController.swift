1ec98d58d23276ac3ab570e85ab4116b7cbf47dd
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-08 16:02:22 -0700

    Added UISliders to the ColorView

--- Colorboard/Colorboard/ColorViewController.swift
+++ Colorboard/Colorboard/ColorViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ColorViewController: UIViewController {
 
+    @IBOutlet weak var textfield: UITextField
+    @IBOutlet weak var redSlider: UISlider
+    @IBOutlet weak var greenSlider: UISlider
+    @IBOutlet weak var blueSlider: UISlider
+
     @IBAction func dismiss(sender: UIBarButtonItem) {
         presentingViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func changeColor(sender: UISlider) {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
