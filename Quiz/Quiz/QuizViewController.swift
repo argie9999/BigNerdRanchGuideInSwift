aa795410f913b2e4f4ab4ed085a5c81ee53e69c1
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-08 02:40:39 -0700

    Start to add some animations to Quiz for Chapter 27 silver challenge.

--- Quiz/Quiz/QuizViewController.swift
+++ Quiz/Quiz/QuizViewController.swift
@@ -13,18 +13,38 @@ class QuizViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
     @IBAction func showQuestion(sender: AnyObject) {
         currentQuestionIndex++
         if currentQuestionIndex == questions.count {
             currentQuestionIndex = 0
         }
+
+        // Silver challenge: Animate the new question coming in from the left
+        questionLabel.frame = CGRectMake(-200, 20, view.bounds.width, 44)
+        questionLabel.alpha = 0.0
+        UIView.animateWithDuration(0.8) {
+            self.questionLabel.frame = CGRectMake(0, 20, self.view.bounds.width, 44)
+            self.questionLabel.alpha = 1.0
+        }
+
         questionLabel.text = questions[currentQuestionIndex]
         answerLabel.text = "???"
+
     }
 
     @IBAction func showAnswer(sender: AnyObject) {
         if questionLabel.text != "" {
             answerLabel.text = answers[currentQuestionIndex]
+            answerLabel.frame = CGRectMake(-200, self.view.bounds.height - 90, view.bounds.width, 44)
+            UIView.animateWithDuration(0.8) {
+                self.answerLabel.frame = CGRectMake(0, self.view.bounds.height - 90,
+                    self.view.bounds.width, 44)
+                self.answerLabel.alpha = 1.0
+            }
         }
     }
 }
