a5be608a7368bb7843c220859406a2246a9455c8
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-08 01:29:25 -0700

    Add some basic animations to HypnoNerd

--- HypnoNerd/HypnoNerd/HypnosisViewController.swift
+++ HypnoNerd/HypnoNerd/HypnosisViewController.swift
@@ -61,6 +61,28 @@ class HypnosisViewController: UIViewController, UITextFieldDelegate {
 
             view.addSubview(messageLabel)
 
+            // Set the labels initial alpha
+            messageLabel.alpha = 0.0
+
+            // Animate the alpha to 1.0
+            UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseIn, animations: {
+                messageLabel.alpha = 1.0
+                }, completion: nil)
+
+            // Animate the center of the labels first to the middle of the screen and then
+            // to a random position on the screen
+            UIView.animateKeyframesWithDuration(1.0, delay: 0.0, options: nil, animations: {
+                UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.8) {
+                    messageLabel.center = self.view.center
+                }
+
+                UIView.addKeyframeWithRelativeStartTime(0.8, relativeDuration: 0.2) {
+                    let x = UInt32(arc4random()) % UInt32(width)
+                    let y = UInt32(arc4random()) % UInt32(height)
+                    messageLabel.center = CGPointMake(CGFloat(x), CGFloat(y))
+                }
+                }) { Bool -> Void in println("Animation finished") }
+
             // Add parallax effect along the horizontal axis
             var motionEffect = UIInterpolatingMotionEffect(keyPath: "center.x",
                 type: UIInterpolatingMotionEffectType.TiltAlongHorizontalAxis)
