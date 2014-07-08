e42b2553225187fd526e5d1179b0725e77f18ae9
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-08 03:17:56 -0700

    Add Colorboard.

---
+++ Colorboard/Colorboard/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Colorboard
+//
+//  Created by Akshay Hegde on 7/8/14.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    func application(application: UIApplication!,
+        didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool
+    {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
