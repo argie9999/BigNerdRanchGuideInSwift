96235ccd79aaed95cec10e858f1935589cd11129
Akshay Hegde <akshay.hegde17@gmail.com>
2014-07-21 19:40:16 -0700

    Update Quiz for Xcode DP4

--- Quiz/Quiz/QuizViewController.swift
+++ Quiz/Quiz/QuizViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-    @IBOutlet weak var questionLabel: UILabel
-    @IBOutlet weak var answerLabel: UILabel
+    @IBOutlet weak var questionLabel: UILabel?
+    @IBOutlet weak var answerLabel: UILabel?
     var currentQuestionIndex: Int = 0
     var questions: [String]
     var answers: [String]
@@ -24,26 +24,25 @@ class QuizViewController: UIViewController {
         }
 
         // Silver challenge: Animate the new question coming in from the left
-        questionLabel.frame = CGRectMake(-200, 20, view.bounds.width, 44)
-        questionLabel.alpha = 0.0
+        questionLabel!.frame = CGRectMake(-200, 20, view.bounds.width, 44)
+        questionLabel!.alpha = 0.0
         UIView.animateWithDuration(0.8) {
-            self.questionLabel.frame = CGRectMake(0, 20, self.view.bounds.width, 44)
-            self.questionLabel.alpha = 1.0
+            self.questionLabel!.frame = CGRectMake(0, 20, self.view.bounds.width, 44)
+            self.questionLabel!.alpha = 1.0
         }
 
-        questionLabel.text = questions[currentQuestionIndex]
-        answerLabel.text = "???"
-
+        questionLabel!.text = questions[currentQuestionIndex]
+        answerLabel!.text = "???"
     }
 
     @IBAction func showAnswer(sender: AnyObject) {
-        if questionLabel.text != "" {
-            answerLabel.text = answers[currentQuestionIndex]
-            answerLabel.frame = CGRectMake(-200, self.view.bounds.height - 90, view.bounds.width, 44)
+        if questionLabel!.text != "" {
+            answerLabel!.text = answers[currentQuestionIndex]
+            answerLabel!.frame = CGRectMake(-200, self.view.bounds.height - 90, view.bounds.width, 44)
             UIView.animateWithDuration(0.8) {
-                self.answerLabel.frame = CGRectMake(0, self.view.bounds.height - 90,
+                self.answerLabel!.frame = CGRectMake(0, self.view.bounds.height - 90,
                     self.view.bounds.width, 44)
-                self.answerLabel.alpha = 1.0
+                self.answerLabel!.alpha = 1.0
             }
         }
     }
